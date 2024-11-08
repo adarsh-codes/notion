0613ea1921875f02f83cb6865d9a13f25ee2cee8
adarsh <adarshksingh14@gmail.com>
2024-11-08 15:22:16 +0530

    1

---
+++ src/data/navbar-links.js
@@ -0,0 +1,22 @@
+export const NavbarLinks = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Catalog",
+    // path: '/catalog',
+  },
+  {
+    title: "About Us",
+    path: "/about",
+  },
+  {
+    title: "Contact Us",
+    path: "/contact",
+  },
+  {
+    title: "Try Code-It-Up",
+    path: "https://code-it-up.netlify.app/"
+  }
+];
